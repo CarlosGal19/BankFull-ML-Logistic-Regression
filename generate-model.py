b460b0cb7124d3be2ab399c18314228c100984a6
Carlos Galindo <utm22030587@utma.edu.mx>
2025-10-25 22:38:36 -0600

    feat: optimize threshold

--- generate-model.py
+++ generate-model.py
@@ -2,26 +2,32 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report, precision_recall_curve
 import joblib
 import datetime
+import numpy as np
 
 df = pd.read_csv('./cleaned_bank_data.csv')
 
 X = df.drop('Target', axis=1)
 y = df['Target']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
 
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
-model = LogisticRegression(max_iter=1000)
+model = LogisticRegression(max_iter=500, class_weight='balanced', random_state=42)
 model.fit(X_train_scaled, y_train)
 
+y_proba = model.predict_proba(X_test_scaled)[:, 1]
+precision, recall, thresholds = precision_recall_curve(y_test, y_proba)
 
-y_pred = model.predict(X_test_scaled)
+f1_scores = 2 * (precision * recall) / np.maximum((precision + recall), 1e-6)
+best_threshold = thresholds[np.argmax(f1_scores)]
+
+y_pred = (y_proba >= best_threshold).astype(int)
 
 print("Accuracy:", accuracy_score(y_test, y_pred))
 print("\nConfusion Matrix:\n", confusion_matrix(y_test, y_pred))
